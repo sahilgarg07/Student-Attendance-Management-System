2ef70d4f424d17e1256bcc7950475b0ec2c51f96
Rohan Manro <99608412+rookyCIPHER@users.noreply.github.com>
2023-12-24 13:16:29 +0530

    Delete Backend directory

--- Backend/Face_Recognation/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
- path('register_image', views.Register_Image, name='Register_image'),
-]
--- Backend/README.md
+++
@@ -1,2 +0,0 @@
-# AttendanceAppServer
- Django Server For Application 
