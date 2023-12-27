ec4d71ef0998fe237a615bed15fa2b5f2e095ce3
Rohan Manro <99608412+rookyCIPHER@users.noreply.github.com>
2023-12-27 15:57:35 +0530

    Add files via upload

---
+++ Backend/Face_Recognation/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+ path('register_image', views.Register_Image, name='Register_image'),
+ path('image_verification', views.image_verification, name='image_verification'),
+]
